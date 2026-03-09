a5c2fd357ef8b15ae7573979d792bff183cb15c9
CAU� DOS SANTOS SIQUEIRA <25170108@educ123.sp.senai.br>
2026-03-09 10:06:25 -0300

    Correção estrutural backend

--- integrador/backend/migrations/20251028_001_create_database.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Criar banco de dados produtos_api
--- Data: 2025-01-15
--- Descrição: Criação inicial do banco de dados
-
-CREATE DATABASE IF NOT EXISTS produtos_api;
-
--- Usar o banco de dados criado
-USE produtos_api;
-
