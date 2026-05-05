62a80511bbd2fd5793e911b7aa06b0927a9aed32
MARINA MONTAGNINI <25170407@educ123.sp.senai.br>
2026-05-05 09:50:44 -0300

    paginas mecanico epi

--- integrador/frontend/src/components/SidebarMecanico/sidebarmecanico.jsx
+++ integrador/frontend/src/components/SidebarMecanico/sidebarmecanico.jsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { PackageOpen, Wrench, History, AlertOctagon, HardHat } from 'lucide-react';
+import { PackageOpen, Wrench, History, AlertOctagon, HardHat, Shield  } from 'lucide-react';
 import SidebarBase from '@/components/SidebarBase/sidebarbase';
 
 const navItems = [
   { href: '/Mecanico/Dashboard', label: 'Minhas Retiradas', icon: PackageOpen, exact: true },
-  { href: '/Mecanico/Epis', label: " Segurança (EPI's)", icon: Shield },
+  { href: '/Mecanico/Epis', label: "Segurança (EPI's)", icon: Shield },
   { href: '/Mecanico/Ferramentas', label: 'Ferramentas', icon: Wrench },
   { href: '/Mecanico/Historico', label: 'Histórico', icon: History },
   { href: '/Mecanico/Alertas', label: 'Alertas', icon: AlertOctagon },
